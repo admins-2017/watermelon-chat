2d2ab30c4b65404a8a427f1985b0649f4fbe5178
kangdongwei <1067905926@qq.com>
2020-05-25 17:57:23 +0800

    test

---
+++ src/main/java/com/kang/WatermelonChatApplication.java
@@ -0,0 +1,13 @@
+package com.kang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WatermelonChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WatermelonChatApplication.class, args);
+    }
+
+}
